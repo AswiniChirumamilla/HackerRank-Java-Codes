482f008a721ec66e5a2cf55f5a0c46c60d89d2da
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-11 20:04:51 +0530

    Update DateandTime.java

--- DateandTime.java
+++ DateandTime.java
@@ -1,7 +1,5 @@
 The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
-
 You are given a date. You just need to write the method, , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
-
 Example
 The method should return  as the day on that date.
 
