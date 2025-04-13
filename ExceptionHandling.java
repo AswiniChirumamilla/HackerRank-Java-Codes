85a9e7ac8028b20df68ba0a3f9585c8e778b54b5
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-13 19:13:31 +0530

    Update ExceptionHandling.java

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -3,10 +3,8 @@ Exception handling is the process of responding to the occurrence, during comput
 Java has built-in mechanism to handle exceptions. Using the try statement we can test a block of code for errors. The catch block contains the code that says what to do if exception occurs.
 
 This problem will test your knowledge on try-catch block.
-
 You will be given two integers  and  as input, you have to compute . If  and  are not  bit signed integers or if  is zero, exception will occur and you have to report it. Read sample Input/Output to know what to report in case of exceptions.
 Sample Input 0:
-
 10
 3
 Sample Output 0:
