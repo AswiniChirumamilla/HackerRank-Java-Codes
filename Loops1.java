ca73f727c506cc2d62eb3b8885941e1470dc3be6
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-14 19:43:49 +0530

    Update Loops1.java

--- Loops1.java
+++ Loops1.java
@@ -1,11 +1,8 @@
 Objective
 In this challenge, we're going to use loops to help us do some simple math.
-
 Task
 Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
-
 Input Format
-
 A single integer, .
 
 Constraints
